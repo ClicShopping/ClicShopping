9b406a92824b5477e3cccecd14a47e38635b4568
clicshopping <admin@clicshopping.org>
2026-05-24 18:19:24 +0200

    license

---
+++ sources/template/Default/css/english/modules_front_page/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright (c) 2008–2026 Loic Richard
+ *
+ * Licensed under AGPLv3 or commercial license.
+ * See LICENSE file.
+ */
+
+
+header("Location: https://" . $_SERVER['HTTP_HOST'] . "/");
